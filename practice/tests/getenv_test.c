a28a4c960c4875324f4d653f057079597a2b7d76
Queenette23 <97518755+Queenette23@users.noreply.github.com>
2023-10-27 14:14:24 +0100

    Delete practice directory

--- practice/tests/getenv_test.c
+++
@@ -1,9 +0,0 @@
-#include "../shell.h"
-
-int main(int argc, char **argv, char **env)
-{
-  printf("[PATH] %s\n", _getenv("PATH"));
-  printf("[PATH] %s\n", _getenv("PATH"));
-  printf("[USER] %s\n", _getenv("USER"));
-  printf("[USER] %s\n", _getenv("USER"));
-}
